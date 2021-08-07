625419cce0cb77e5e873dfbcf3be0bc6dabc0651
Theosaurus-Rex <theosaurus13@gmail.com>
2021-08-07 10:26:59 +1000

    can now call weather info; still rendering break tags

---
+++ src/components/CurrentForecast.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import GetWeather from '../config/GetWeather'
+
+const CurrentForecast = () => {
+    
+    return <GetWeather />
+}
+
+export default CurrentForecast
---
+++ src/config/GetWeather.jsx
@@ -0,0 +1,37 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import {decode} from 'html-entities'
+
+
+
+const GetWeather = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [weather, setWeather] = useState([]);
+  
+    useEffect(() => {
+      axios("https://api.tfl.gov.uk/AirQuality/")
+        .then(
+          res => setWeather(decode(res.data.currentForecast[0].forecastText))
+        )
+        .catch((error) => {
+          setIsLoaded(true);
+          setError(error);
+        })
+        .finally(
+          setIsLoaded(true),
+          setWeather(decode(weather)))
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <p>{weather}</p>
+      );
+    }
+  }
+
+  export default GetWeather
