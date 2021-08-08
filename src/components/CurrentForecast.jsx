ea748b1ce6f379f4b9e31c1164c43ce1054ef02c
Theosaurus-Rex <theosaurus13@gmail.com>
2021-08-08 10:47:30 +1000

    remove elements that were redundant

--- src/components/CurrentForecast.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import GetCurrentWeather from '../config/GetCurrentWeather'
-
-const CurrentForecast = () => {
-    
-    return <GetCurrentWeather  />
-}
-
-export default CurrentForecast
